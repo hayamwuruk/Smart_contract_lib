bf027f7e1b4f80efbbb8db28934ef606d79236ad
hayamwuruk <rafibuss4@gmail.com>
2024-09-20 13:58:28 +0700

    Smart_contrac_Basic

---
+++ smart contract/lottery-react 4/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import web3 from "./web3";
+import lottery from "./lottery";
+
+class App extends React.Component {
+  state = {
+    manager: "",
+    players: [],
+    balance: "",
+  };
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>
+          This contract is managed by {this.state.manager}. There are currently{" "}
+          {this.state.players.length} people entered, competing to win{" "}
+          {web3.utils.fromWei(this.state.balance, "ether")} ether!
+        </p>
+      </div>
+    );
+  }
+}
+export default App;
